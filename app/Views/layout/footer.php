6c1c581d453475b14c1b3dd286ede8a3277e8183
ahmadsofuwan <69912778+ahmadsofuwan@users.noreply.github.com>
2022-08-31 20:26:35 +0700

    update js

---
+++ app/Views/layout/footer.php
@@ -0,0 +1,13 @@
+</body>
+
+
+<!-- plugin for charts  -->
+<script src="<?= base_url('asset/soft-ui/build/') ?>/assets/js/plugins/chartjs.min.js" async></script>
+<!-- plugin for scrollbar  -->
+<script src="<?= base_url('asset/soft-ui/build/') ?>/assets/js/plugins/perfect-scrollbar.min.js" async></script>
+<!-- github button -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<!-- main script file  -->
+<script src="<?= base_url('asset/soft-ui/build/') ?>/assets/js/soft-ui-dashboard-tailwind.js?v=1.0.4" async></script>
+
+</html>
